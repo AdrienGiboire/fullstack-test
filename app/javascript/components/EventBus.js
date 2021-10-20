0b813cd9c6336699024942823d99ea3234fdfe9f
Adrien <github@adrien.sent.com>
2021-10-20 17:15:27 +0200

    style: remove debug instructions

--- app/javascript/components/EventBus.js
+++ app/javascript/components/EventBus.js
@@ -1,11 +1,9 @@
 const EventBus = {
   on (event, callback) {
-    console.debug('listening on ', event)
     document.addEventListener(event, (e) => callback(e.detail))
   },
 
   emit (event, data) {
-    console.debug('emitting ', event)
     document.dispatchEvent(new CustomEvent(event, { detail: data }))
   }
 }
