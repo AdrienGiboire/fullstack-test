1ec7298873afdaba9040a19de8f3549407ac970c
Adrien <github@adrien.sent.com>
2021-11-04 09:05:40 +0100

    fix: search for multiple ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,15 +4,11 @@ class Recipe < ApplicationRecord
 
     ingredients = [ingredients] unless ingredients.is_a?(Array)
 
-    sub_where_clause = ""
+    where_clause = ""
     ingredients.each do |ingredient|
-      sub_where_clause += if sub_where_clause.empty? then
-                        "ingredient ILIKE '%#{ingredient}%'"
-                      else
-                        "AND ingredient ILIKE '%#{ingredient}%'"
-                      end
+      where_clause += " AND " unless where_clause.empty?
+      where_clause += "EXISTS (SELECT FROM unnest(ingredients) ingredient WHERE 1=1 AND ( ingredient ILIKE '%#{ingredient}%' ))"
     end
-    where_clause = "EXISTS (SELECT FROM unnest(ingredients) ingredient WHERE 1=1 AND ( #{sub_where_clause} ))"
 
     where(where_clause)
   end
